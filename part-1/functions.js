0b922aa1db49d84fc9288de16ff3ac680ff8140d
James McCormack <thejamesmccormack@gmail.com>
2017-09-05 12:44:09 -0700

    added filterBetween function and tests

--- part-1/functions.js
+++ part-1/functions.js
@@ -106,6 +106,25 @@ function nameProps(obj) {
   }
 }
 
-console.log(nameProps({ pet: "cats", cookie: "oatmeal raisin", fruit: "bananas" }));
+ // Write a function filterBetween(array, min, max) that takes an array of strings, a min value, and a max value. It returns a new array containing only the elements that come after min alphabetically and before max.
 
-module.exports = { month, reverseSentence, nameProps };
+function filterBetween(array, min, max) {
+  if (Array.isArray(array) === false) {
+    throw new TypeError("first argument must be an array");
+  } else if (typeof min !== "string") {
+    throw new TypeError("min must be a string");
+  } else if (typeof max !== "string") {
+    throw new TypeError("max must be a string");
+  }
+  try {
+    const filteredArray = array.filter(function (word) {
+      return word >= min && word <= max;
+    });
+    return filteredArray;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+module.exports = { month, reverseSentence, nameProps, filterBetween };
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { month, reverseSentence, nameProps } = require('./functions.js');
+const { month, reverseSentence, nameProps, filterBetween } = require('./functions.js');
 
 describe('month()', function () {
   it('should return september when passed new Date(1981, 8, 24)', function () {
@@ -38,5 +38,21 @@ describe('nameProps()', function () {
   });
   it('should throw an error if passed an array', function () {
     expect(() => nameProps(["bananas", "cats", "Fiji"])).to.throw(TypeError, "not an object");
+  });
+});
+
+describe('filterBetween()', function () {
+  it('should return ["dates", "kiwis"] if min is "cherries" and max is "strawberries"', function () {
+    const fruitArray = ["apples", "bananas", "dates", "kiwis", "watermelons"];
+    expect(filterBetween(fruitArray, "cherries", "strawberries")).to.eql(["dates", "kiwis"]);
+  });
+  it('should throw an error if first argument is not an array', function () {
+    expect(() => filterBetween("fakeArray", "elephant", "kazoo")).to.throw(TypeError, "first argument must be an array");
+  });
+  it('should throw an error if min is not a string', function () {
+    expect(() => filterBetween(["apples", "bananas"], 5, "strawberries")).to.throw(TypeError, "min must be a string");
+  });
+  it('should throw an error if max is a not a string', function () {
+    expect(() => filterBetween(["apples", "bananas"], "cherries", 7)).to.throw(TypeError, "max must be a string");
   });
 });
